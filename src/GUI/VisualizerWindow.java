d7720248ece74ca96004fd699eaa5fcb2f5320eb
vaporbolt <vaporbolt@gmail.com>
2022-01-11 18:18:07 -0500

    Basic classes for git test.

---
+++ src/GUI/VisualizerWindow.java
@@ -0,0 +1,24 @@
+package GUI;
+import javax.swing.*;
+
+/**
+ * @author Seth Roper
+ * 
+ * Encompasses the construction of the JWindow.
+ *
+ */
+public class VisualizerWindow {
+  
+  private JWindow window;
+  
+  
+  /**
+   *  private constructor for the Jwindow used by the windowFactory.
+   */
+  private VisualizerWindow() 
+  {
+    JWindow window = new JWindow();
+  }
+  
+
+}
---
+++ src/driver/Driver.java
@@ -0,0 +1,17 @@
+package driver;
+
+/**
+ * @author Seth Roper
+ * @version 1/11/2022
+ *  
+ *  Driver class for the merge sort visualizer.
+ *
+ */
+public class Driver {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
